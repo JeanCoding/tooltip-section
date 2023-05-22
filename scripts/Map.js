220b66c37ac6dceaca2d09717823188863a44872
Jean Kalo <jean.kalo@student.rocva.nl>
2023-05-22 21:07:07 +0200

    Add: new comments to the JavaScript

--- scripts/Map.js
+++ scripts/Map.js
@@ -2,23 +2,22 @@ import tippy from 'tippy.js';
 
 class Map {
 
+  // Creëert een Map object met 'node' en activeert Tippy.
   constructor(node) {
 
     this.map = node;
     this.tippyContainer = node.querySelector('.tooltip-container');
 
     this.initTippy();
-
-
   }
 
   initTippy() {
 
+    // Maakt een variabele van het huidige object (wordt toegankelijk in de functies/scope's)
     let self = this;
 
-    // Desktop tippy
+    // Settings tippy voor desktoplocaties
     tippy(self.map.querySelectorAll('.location.desktop'), {
-
       allowHTML: true,
       hideOnClick: true,
       trigger: "click",
@@ -29,15 +28,15 @@ class Map {
       appendTo: self.tippyContainer,
 
       content(reference) {
+        // Haalt de waarde op van de locatie
         const key = reference.getAttribute('data-location-key');
+        // Zoekt de bijbehorende tooltip op basis van de waarde en retourneer de inhoud
         return self.map.querySelector(`.tooltip[data-location-key="${key}"]`).innerHTML;
       },
     });
 
-
-    // Mobile tippy
+    // Settings tippy voor mobiele locaties
     tippy(self.map.querySelectorAll('.location.mobile'), {
-
       allowHTML: true,
       hideOnClick: true,
       trigger: "click",
@@ -47,25 +46,26 @@ class Map {
       delay: 0,
 
       content(reference) {
+        // Haalt de sleutelwaarde op van de locatie
         const key = reference.getAttribute('data-location-key');
+        // Zoekt de bijbehorende tooltip op basis van de sleutelwaarde en retourneer de inhoud
         return self.map.querySelector(`.tooltip[data-location-key="${key}"]`).innerHTML;
       },
 
       onMount(instance) {
-
+        // Verwijder alle stijlen van de popper om problemen met de layout te voorkomen
         instance.popper.removeAttribute('style');
-
-
       },
 
       popperOptions: {
         modifiers: [{
           name: 'eventListeners',
           options: {
-            scroll: false,
+            scroll: false, // Schakelt het scrollgedrag van de popper uit
           },
-        }, ],
+        }],
       },
+      
     });
 
   }
